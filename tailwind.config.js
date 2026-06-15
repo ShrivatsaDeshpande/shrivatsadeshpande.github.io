39212c6d3ffdc5f595ddbd1df5c699ebe9beec74
CyberSavvy <115502753+ShrivatsaDeshpande@users.noreply.github.com>
2026-06-15 03:19:05 +0100

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   darkMode: 'class',
   theme: {
-    fontFamily: {
-      sans: ['"Anonymous Pro"', 'monospace'],
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        glass: 'rgba(255, 255, 255, 0.03)',
+        glassBorder: 'rgba(255, 255, 255, 0.08)',
+      }
     },
   },
   plugins: [flowbite.plugin()],
